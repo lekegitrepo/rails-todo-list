c429313426c635854ebdf6e50f668cf707186d85
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-02-28 12:16:39 +0100

    Add task form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,21 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ['Reading', 'Writing', 'Creating', 'Washing']
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
