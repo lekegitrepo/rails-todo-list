b5b74ff3695009388722d2d1e79c5c1c63fbb6a3
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-03-02 11:57:06 +0100

    Create session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  resources :users
   get 'welcome/index'
   root 'welcome#index'
 
-  get 'tasks/:id/complete/:completed', to: 'tasks#complete'
+  get 'log_in', to: 'sessions#new'
+  get 'log_out', to: 'sessions#destroy'
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :users
+  get 'sign_up', to: 'users#new'
 
   resources :tasks
+
+  get 'tasks/:id/complete/:completed', to: 'tasks#complete'
 end
